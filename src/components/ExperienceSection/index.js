fea9a06d57e63651d15a4a2a943c9530ad2adbbf
_vijaykolar <vijay@indivirtualdubai.com>
2018-06-08 13:59:40 +0530

    Updated experience section

--- src/components/ExperienceSection/index.js
+++ src/components/ExperienceSection/index.js
@@ -12,12 +12,12 @@ class ExperienceSection extends React.Component {
       <Section title="Experience">
         <div className="row">
           <ExperienceUnit
-            logo={GLobussoft}
-            colour='#ffffff'
-            title='Globussoft'
-            link='https://globussoft.com'
-            timeperiod='2016'
-            subtitle='Worked as UI design cum developer. Designed Android and iOS apps using photoshop.'
+            logo={iv}
+            colour='#ff4f00'
+            title='Indivirtual Dubai'
+            link='https://www.indivirtualdubai.com/'
+            timeperiod='2018'
+            subtitle='Working as a UI developer on current technologies like React'
           />
           <ExperienceUnit
             logo={lollypop}
@@ -28,13 +28,15 @@ class ExperienceSection extends React.Component {
             subtitle='Worked as UI developer. Built websites using Pug, Scss, JS, and Gulp.'
           />
           <ExperienceUnit
-            logo={iv}
-            colour='#ff4f00'
-            title='Indivirtual Dubai'
-            link='https://www.indivirtualdubai.com/'
-            timeperiod='2018'
-            subtitle='Working as a UI developer on current technologies like React'
+            logo={GLobussoft}
+            colour='#ffffff'
+            title='Globussoft'
+            link='https://globussoft.com'
+            timeperiod='2016'
+            subtitle='Worked as UI design cum developer. Designed Android and iOS apps using photoshop.'
           />
+          
+          
         </div>
       </Section>
     )
