f937e40ba4b3da446cff4ad2a6a03e782ce57016
Vijay Kolar <vijayikolar@gmail.com>
2021-05-13 11:12:10 +0530

    Updated js file

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 const about = document.getElementById("about");
-const contact = document.getElementById("contact");
 const aboutContent = document.getElementById("about-content");
-const contactContent = document.getElementById("contact-content");
 
 about.addEventListener("click", () => {
   const about = new WinBox({
@@ -11,23 +9,6 @@ about.addEventListener("click", () => {
     bottom: 40,
     left: 40,
     mount: aboutContent,
-    onfocus: function () {
-      this.setBackground("#0a0");
-    },
-    onblur: function () {
-      this.setBackground("#777");
-    },
-  });
-});
-
-contact.addEventListener("click", () => {
-  const contact = new WinBox({
-    title: "About me",
-    top: 40,
-    right: 40,
-    bottom: 40,
-    left: 40,
-    mount: contactContent,
     onfocus: function () {
       this.setBackground("#0a0");
     },
